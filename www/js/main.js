388d84cde9291173473e006b67781eb59158b781
William Okuno <beastyworld@gmail.com>
2015-10-14 22:38:32 -0500

    unregisters device

--- www/js/main.js
+++ www/js/main.js
@@ -156,6 +156,7 @@ function bannerset(activepage, stackids){
 }
 //self explanatory figure it out, login.html wipes all localdata
 function logout(){
+    Pushbots.sharedInstance().unRegister();
     localStorage.clear();
     document.location.href = 'login.html';
 }
