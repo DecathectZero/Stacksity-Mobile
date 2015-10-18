149b70cc819b1ea2a1d7e4a1b114b3cb05341dfa
Tyler Han <tylerhan97@gmail.com>
2015-10-18 18:15:01 -0400

    more attempts at double tap

--- www/js/main.js
+++ www/js/main.js
@@ -897,27 +897,35 @@ function commentHTML(element, depth){
     '</div> </div> </div>';
 }
 
-(function($) {
-    $.fn.doubleTap = function(doubleTapCallback) {
-        return this.each(function(){
-            var elm = this;
-            var lastTap = 0;
-            $(elm).bind('vmousedown', function (e) {
-                var now = (new Date()).valueOf();
-                var diff = (now - lastTap);
-                lastTap = now ;
-                if (diff < 250) {
-                    if($.isFunction( doubleTapCallback ))
-                    {
-                        doubleTapCallback.call(elm);
-                    }
-                }
-            });
-        });
-    }
-})(jQuery);
+jQuery.event.special.dblclick = {
+    setup: function(data, namespaces) {
+        var elem = this,
+            $elem = jQuery(elem);
+        $elem.bind('touchend.dblclick', jQuery.event.special.dblclick.handler);
+    },
 
-$(document).on("doubleTap", ".comment-content", function (event) {
+    teardown: function(namespaces) {
+        var elem = this,
+            $elem = jQuery(elem);
+        $elem.unbind('touchend.dblclick');
+    },
+
+    handler: function(event) {
+        var elem = event.target,
+            $elem = jQuery(elem),
+            lastTouch = $elem.data('lastTouch') || 0,
+            now = new Date().getTime();
+
+        var delta = now - lastTouch;
+        if(delta > 20 && delta<500){
+            $elem.data('lastTouch', 0);
+            $elem.trigger('dblclick');
+        }else
+            $elem.data('lastTouch', now);
+    }
+};
+
+$(document).on("dblclick", ".comment-content", function (event) {
     event.preventDefault();
 
     // Set up collapse state variables to keep track of opened/closed comments
