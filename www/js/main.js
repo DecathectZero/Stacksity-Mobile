ed0834fc5fc5c60ca79071f520eedf16e2de95cf
Tyler Han <tylerhan97@gmail.com>
2015-10-21 01:41:47 -0400

    test

--- www/js/main.js
+++ www/js/main.js
@@ -769,7 +769,7 @@ $(document).on('tap','a',function(e){
         }else if($(this).hasClass("sharelink")){
             e.preventDefault();
             var goto = $(this).data('link');
-            window.plugins.socialsharing.share(null, null, null, "https://stacksity.com/p/" + goto);
+            window.plugins.socialsharing.share('Message and link', null, null, 'http://www.x-services.nl');
         }else{
             var link = $(this).attr("href");
             if(link==null||$(this).hasClass("ui-input-clear")){
