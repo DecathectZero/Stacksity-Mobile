91ed0626fc5e1cd05bd1ab464bfc3ba808d4319b
Tyler Han <tylerhan97@gmail.com>
2015-10-21 22:40:44 -0400

    adsadsa

--- www/js/main.js
+++ www/js/main.js
@@ -738,7 +738,7 @@ function imgurupload(fileURL) {
         ///* And now, we send the formdata */
         //xhr.send(fd);
 
-        $("#imagespinner").show();
+        alert(fileURL);
         var ft = new FileTransfer();
         var headers = {'Authorization':'Client-ID 2caf3e86e092d76'};
         var options = new FileUploadOptions();
